672b38a163254225bba0771b31dff66ab2b17f22
Akhil Biju <bijuakhil260260@gmail.com>
2023-10-03 08:11:22 +0530

    Premium page design completed

--- lib/view/bottomnavigation/home/premium.dart
+++ lib/view/bottomnavigation/home/premium.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/model/color_constant/colors.dart';
+import 'package:spotify_clone/view/bottomnavigation/home/premium_constant/plancontainer.dart';
 
 class Premium extends StatelessWidget {
   const Premium({super.key});
@@ -8,109 +9,333 @@ class Premium extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundcolor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * .3,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                      "assets/images/pre.webp",
-                    ),
-                    fit: BoxFit.cover)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0, top: 15),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * .08,
-                    width: MediaQuery.of(context).size.width * .09,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "assets/images/Spotiy Logo.png",
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * .3,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "assets/images/pre.webp",
+                      ),
+                      fit: BoxFit.cover)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0, top: 15),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * .08,
+                      width: MediaQuery.of(context).size.width * .09,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/images/Spotiy Logo.png",
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0, left: 10),
-                  child: Text("Premium",
-                      style: TextStyle(
-                          color: backgroundcolor,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold)),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text("FREE TRIAL",
-                style: TextStyle(
-                    color: textcolor,
-                    fontSize: 10,
-                    fontWeight: FontWeight.w400)),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text("Try Premium for 1 month",
-                style: TextStyle(
-                    color: textcolor,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold)),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Center(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * .06,
-              width: MediaQuery.of(context).size.width * .9,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "GET PREMIUM",
-                  style: TextStyle(
-                      color: backgroundcolor,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
-                ),
-                style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(), backgroundColor: textcolor),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0, left: 10),
+                    child: Text("Premium",
+                        style: TextStyle(
+                            color: backgroundcolor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
               ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: RichText(
-                text: TextSpan(style: TextStyle(color: Colors.grey), 
-                children: [
-              TextSpan(
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("FREE TRIAL",
+                  style: TextStyle(
+                      color: textcolor,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("Try Premium for 1 month",
+                  style: TextStyle(
+                      color: textcolor,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * .06,
+                width: MediaQuery.of(context).size.width * .9,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "GET PREMIUM",
+                    style: TextStyle(
+                        color: backgroundcolor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: StadiumBorder(), backgroundColor: textcolor),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: RichText(
                   text:
-                      "Only ₹119/month after.Offer only for users who are new to premimum."),
-              TextSpan(
-                  text: "Terms and Conditions apply.",
-                  style:
-                      TextStyle(fontWeight: FontWeight.bold, color: textcolor))
-            ])),
-          )
-        ],
+                      TextSpan(style: TextStyle(color: Colors.grey), children: [
+                TextSpan(
+                    text:
+                        "Only ₹119/month after.Offer only for users who are new to premimum.",
+                    style: TextStyle(fontSize: 11)),
+                TextSpan(
+                    text: "Terms and Conditions apply.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: textcolor,
+                        fontSize: 10))
+              ])),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            joinPremiumContainer(context),
+            SizedBox(
+              height: 20,
+            ),
+            spotifyFreeContainer(context),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Text("Pick Your Premium",
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: textcolor,
+                      fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Premiumcontainer(
+                plan: 'Mini',
+                description:
+                    '1 day and 1 week plans • Ad-free music on mobile • Download 30 songs on 1 mobile device • Mobile only plan',
+                color1: Colors.lightBlueAccent,
+                color2: Colors.blueAccent,
+                color3: Colors.blue,
+                color4: const Color.fromARGB(255, 40, 81, 114)),
+            SizedBox(
+              height: 20,
+            ),
+            Premiumcontainer(
+                plan: 'Premium Individual',
+                description:
+                    ' Ad-free music on mobile • Download to listen offline • Debit and credit cards accepted ',
+                color1: const Color.fromARGB(255, 52, 133, 55),
+                color2: Colors.lightGreen,
+                color3: Colors.green,
+                color4: Colors.greenAccent),
+            SizedBox(
+              height: 20,
+            ),
+            Premiumcontainer(
+                plan: 'Premium Duo',
+                description:
+                    ' Ad-free music on mobile • Download to listen offline • Debit and credit cards accepted • Upto 5 devices • Download 10000 songs on 5 mobile devices  ',
+                color1: Color.fromARGB(255, 126, 182, 208),
+                color2: Color.fromARGB(255, 99, 133, 202),
+                color3: Color.fromARGB(255, 58, 109, 145),
+                color4: Color.fromARGB(255, 86, 107, 227)),
+            SizedBox(
+              height: 20,
+            ),
+            Premiumcontainer(
+                plan: 'Premium Family',
+                description:
+                    ' Ad-free music on mobile • Download to listen offline • Debit and credit cards accepted • Upto 5 devices • Download 10000 songs on 5 mobile devices  ',
+                color1: Color.fromARGB(255, 126, 182, 208),
+                color2: Color.fromARGB(255, 163, 90, 190),
+                color3: Color.fromARGB(255, 177, 42, 128),
+                color4: Color.fromARGB(255, 211, 12, 145)),
+            SizedBox(
+              height: 20,
+            ),
+            Premiumcontainer(
+                plan: 'Premium Student',
+                description:
+                    ' Ad-free music on mobile • Download to listen offline • Debit and credit cards accepted • Upto 5 devices • Download 10000 songs on 5 mobile devices  ',
+                color1: Color.fromARGB(255, 232, 148, 13),
+                color2: Color.fromARGB(255, 219, 111, 16),
+                color3: Color.fromARGB(255, 190, 148, 31),
+                color4: Color.fromARGB(255, 224, 150, 21))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Center spotifyFreeContainer(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height * .09,
+        width: MediaQuery.of(context).size.width * .9,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: gridcolor,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(23.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Spotify Free",
+                style: TextStyle(
+                    fontSize: 17,
+                    color: textcolor,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "CURRENT PLAN",
+                style: TextStyle(
+                    fontSize: 12,
+                    color: textcolor,
+                    fontWeight: FontWeight.w500),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Center joinPremiumContainer(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height * .26,
+        width: MediaQuery.of(context).size.width * .9,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: gridcolor,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, top: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Why join Premium?",
+                  style: TextStyle(
+                      color: textcolor,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold)),
+              Divider(
+                thickness: 0.1,
+                color: textcolor,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.done,
+                    color: spotifyGreen,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text("Download to listen offline without wifi",
+                        style: TextStyle(
+                            color: textcolor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.done,
+                    color: spotifyGreen,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text("Music without ad interruptions",
+                        style: TextStyle(
+                            color: textcolor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.done,
+                    color: spotifyGreen,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text("2x higher sound quality than our free plan",
+                        style: TextStyle(
+                            color: textcolor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.done,
+                    color: spotifyGreen,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text("Cancel monthly plans online anytime",
+                        style: TextStyle(
+                            color: textcolor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/view/bottomnavigation/home/premium_constant/plancontainer.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/model/color_constant/colors.dart';
+
+class Premiumcontainer extends StatefulWidget {
+  final String plan;
+  final String description;
+  final Color color1;
+  final Color color2;
+  final Color color3;
+  final Color color4;
+  const Premiumcontainer(
+      {super.key,
+      required this.plan,
+      required this.description,
+      required this.color1,
+      required this.color2,
+      required this.color3,
+      required this.color4});
+
+  @override
+  State<Premiumcontainer> createState() => _PremiumcontainerState();
+}
+
+class _PremiumcontainerState extends State<Premiumcontainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height * .3,
+        width: MediaQuery.of(context).size.width * .9,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: gridcolor,
+            gradient: LinearGradient(
+              colors: [
+                widget.color1,
+                widget.color2,
+                widget.color3,
+                widget.color4,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.plan,
+                    style: TextStyle(color: textcolor, fontSize: 16),
+                  ),
+                  Column(
+                    children: [
+                      Text("From ₹7",
+                          style: TextStyle(
+                              color: textcolor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                      Text("FOR 1 DAY",
+                          style: TextStyle(
+                            color: textcolor,
+                            fontSize: 10,
+                          ))
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                widget.description,
+                style: TextStyle(
+                  color: textcolor,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 50,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("VIEW PLANS",
+                      style: TextStyle(
+                          color: backgroundcolor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                  style: ElevatedButton.styleFrom(
+                      shape: StadiumBorder(), backgroundColor: textcolor),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RichText(
+                  text: TextSpan(
+                      style: TextStyle(
+                        color: gridcolor,
+                      ),
+                      children: [
+                    TextSpan(
+                        text: "Only for users who are new to Premium. ",
+                        style: TextStyle(fontSize: 10, color: gridcolor)),
+                    TextSpan(
+                        text: "Terms and Conditions apply.",
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: textcolor,
+                            fontWeight: FontWeight.bold))
+                  ]))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
